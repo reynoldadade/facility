cafb21886b1d34b48dbdc337097a174c36f05acb
Reynold Osei Adade <reynoldadade@gmail.com>
2020-11-19 12:40:03 +0000

    list of buildings

--- components/buildings/ListOfBuildings.js
+++ components/buildings/ListOfBuildings.js
@@ -1,6 +1,46 @@
+const buildingsTemplates = [
+  {
+    nameOfBuilding: '14 Mango Tree Avenue',
+    numberOfRooms: 10,
+    numberOfTenants: 5,
+  },
+  {
+    nameOfBuilding: '14 Mango Tree Avenue',
+    numberOfRooms: 10,
+    numberOfTenants: 5,
+  },
+  {
+    nameOfBuilding: '14 Mango Tree Avenue',
+    numberOfRooms: 10,
+    numberOfTenants: 5,
+  },
+];
+
 const ListOfBuildings = (props) => (
   <div className="p-2">
-    <div className="pt-1 border-2 border-gray-700">Name</div>
+    {buildingsTemplates.map((building) => (
+      <div className=" mt-1 p-1 flex flex-col bg-white shadow-sm text-sm text-gray-900">
+        <div>
+          <span className="uppercase text-gray-500 text-label">address</span>
+
+          <span className=""> {building.nameOfBuilding}</span>
+        </div>
+        <div>
+          <span className="uppercase text-gray-500 text-label">
+            number of rooms
+          </span>
+
+          <span className=""> {building.numberOfRooms}</span>
+        </div>
+        <div>
+          <span className="uppercase text-gray-500 text-label">
+            number of tenants
+          </span>
+
+          <span className=""> {building.numberOfTenants}</span>
+        </div>
+      </div>
+    ))}
   </div>
 );
 
--- components/dashboardComponent.js
+++ components/dashboardComponent.js
@@ -1,11 +1,11 @@
 import ListOfBuildings from './buildings/ListOfBuildings';
 const dashboard = (props) => (
   <div className="grid grid-rows-3 grid-flow-col gap-4 p-4">
-    <div className="row-span-3 bg-gray-300">
+    <div className="row-span-3 bg-gray-200">
       <ListOfBuildings />
     </div>
-    <div className="row-span-1 col-span-2 bg-gray-200">breadcrumbs</div>
-    <div className="row-span-2 col-span-2 bg-gray-200"></div>
+    <div className="row-span-1 col-span-4 bg-gray-200">breadcrumbs</div>
+    <div className="row-span-2 col-span-4 bg-gray-200"></div>
   </div>
 );
 
